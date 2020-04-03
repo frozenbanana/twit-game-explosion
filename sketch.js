6f5f19ea7ea680d48b2afe8a2cce51d029a1354e
Henry Bergström <user@Henrys-iMac.local>
2020-04-03 12:19:07 +0200

    Playing around with circles and names.

--- sketch.js
+++ sketch.js
@@ -11,25 +11,36 @@ const getTwitterData = async () => {
         data = json.data;
     }); //extract JSON from the http response
 };
+
 console.log("Trying to get data...");
 getTwitterData();
 
 function setup() {
     createCanvas(640, 400);
-    background(color(255, 0, 0));
+    background(color(255, 150, 150));
 }
 
 function draw() {
     //console.log("The data is: ", data);
-    let x = 0;
+    let x = 25;
     let y = random(0, 400);
-    fill(color(random(0, 255), random(0, 255), random(0, 255)));
+    let cirlceColor = color(random(0, 255), random(0, 255), random(0, 255));
     //console.log("data is: ", data);
     if (data.length > 0) {
-        console.log(data[i]);
-
         for (let i = 0; i < data.length; i++) {
-            ellipse(x + i * 25, y, 25, 25);
+            console.log(data[i].employee_name);
+            let circleSize = data[i].employee_age / 2;
+            fill(cirlceColor);
+            ellipse(x + i * 25, y, circleSize, circleSize);
+            textSize(10);
+            fill(color("BLACK"));
+            let yOffset = (i % 2) * circleSize + 20;
+            let xOffset = 10;
+            let first_name = data[i].employee_name.substr(
+                0,
+                data[i].employee_name.indexOf(" ")
+            );
+            text(first_name, x + i * 25 - xOffset, y - 45 + yOffset);
         }
         noLoop();
     }
