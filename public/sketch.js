54682416f5638c7c7e493b3fccaacc94dcc5714b
henrybergstrom <henrybergstrom@protonmail.com>
2020-04-07 21:14:52 +0200

    Added showcase of data from api

--- public/sketch.js
+++ public/sketch.js
@@ -12,36 +12,37 @@ const getTwitterData = async () => {
     }); //Extract JSON from the http response
 };
 
+const getApiData = async () => {
+    // Make sure .env file has PORT=3001
+    const response = await fetch(
+        "http://localhost:3001/data"
+    );
+    return await response.json().then(json => {
+        console.log("json has been gathered. Returning data.", json.data);
+        data = json.data;
+    }); //Extract JSON from the http response
+};
 console.log("Trying to get data...");
-getTwitterData();
-//getCryptoData();
+// getTwitterData();
+getApiData();
 
 function setup() {
     createCanvas(640, 400);
-    background(color(255, 150, 150));
+    background(0);
+    textSize(10);
 }
 
 function draw() {
     //console.log("The data is: ", data);
-    let x = 25;
-    let y = random(0, 400);
-    let cirlceColor = color(random(0, 255), random(0, 255), random(0, 255));
+    let x = random(0, width);
+    let y = random(0, height);
     //console.log("data is: ", data);
     if (data.length > 0) {
         for (let i = 0; i < data.length; i++) {
-            console.log(data[i].employee_name);
-            let circleSize = data[i].employee_age / 2;
-            fill(cirlceColor);
-            ellipse(x + i * 25, y, circleSize, circleSize);
-            textSize(10);
-            fill(color("BLACK"));
-            let yOffset = (i % 2) * circleSize + 20;
-            let xOffset = 10;
-            let first_name = data[i].employee_name.substr(
-                0,
-                data[i].employee_name.indexOf(" ")
-            );
-            text(first_name, x + i * 25 - xOffset, y - 45 + yOffset);
+            let col = color(random(100,255), random(100,255), random(100,255));
+            stroke(col);
+            fill(col);
+            text(data[i].name, random(0, width), random(0, height));
         }
         noLoop();
     }
