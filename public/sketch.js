c123cc43c16e6108f653a3918fc67c99b1652155
Henry Bergström <user@Henrys-iMac.local>
2020-04-17 12:03:40 +0200

    We have data from coinmarketcap api and reddit api
    Co-authored-by: Hoxbot <jaqobmansson@gmail.com>
    Co-authored-by: maoe16@student.bth.se <maoe16@student.bth.se>

--- public/sketch.js
+++ public/sketch.js
@@ -1,13 +1,13 @@
 /* Global Variabels */
 // Server call to Twitter API
-let twitter_data = [];
+let reddit_data = { children: [] };
 let api_data = [];
 
 const getTwitterData = async () => {
     const response = await fetch(
         "http://dummy.restapiexample.com/api/v1/employees"
     );
-    return await response.json().then(json => {
+    return await response.json().then((json) => {
         console.log("json has been gathered. Returning data.", json.data);
         twitter_data = json.data;
     }); //Extract JSON from the http response
@@ -15,38 +15,51 @@ const getTwitterData = async () => {
 
 const download = (content, fileName, contentType) => {
     let a = document.createElement("a");
-    let file = new Blob([JSON.stringify(content)], {type: contentType});
+    let file = new Blob([JSON.stringify(content)], { type: contentType });
     a.href = URL.createObjectURL(file);
     a.download = fileName;
     a.click();
-}
-
+};
 
 const getApiData = async (api_url) => {
     let api_link = "http://localhost:3001/" + api_url;
     console.log("Accessing api data for: ", api_link);
-    
+
     // It was a promise, but await-ing it turned it into a Response
     const response = await fetch(api_link);
     // Returning a promise since it says async (api_type)
-    return response.json().then(json => json.data);
-}
+    return response.json().then((json) => json.data);
+};
 
 const getDiskData = () => {
-      return data;
-}
+    return data;
+};
 
 console.log("Trying to get data...");
-let url = "data";
-getApiData(url).then(data => {
-    console.log('We got the data', data);
-    // download(data, 'data.json', 'text/plain');
-    api_data = data;
-}).catch(err => {
-    console.log('Failed to retreive data', err);
-    // if (err.statusCode == xxx) {}
-    api_data = getDiskData('data/data.json');
-});
+
+getApiData("coinmarket?sort=name")
+    .then((data) => {
+        console.log("We got the data", data);
+        // download(data, 'coinmarket.json', 'text/plain');
+        api_data = data;
+    })
+    .catch((err) => {
+        console.log("Failed to retreive data", err);
+        // if (err.statusCode == xxx) {}
+        // api_data = getDiskData("data/data.json");
+    });
+
+getApiData("reddit?subreddit=bitcoin")
+    .then((data) => {
+        console.log("We got the data", data);
+        // download(data, 'reddit.json', 'text/plain');
+        reddit_data = data;
+    })
+    .catch((err) => {
+        console.log("Failed to retreive data", err);
+        // if (err.statusCode == xxx) {}
+        // api_data = getDiskData("data/data.json");
+    });
 
 function setup() {
     createCanvas(640, 400);
@@ -56,15 +69,30 @@ function setup() {
 
 function draw() {
     //console.log("The data is: ", data);
-    let x = random(0, width);
-    let y = random(0, height);
+    // let x = random(0, width);
+    // let y = random(0, height);
     //console.log("data is: ", data);
-    if (api_data.length > 0) {
-        for (let i = api_data.length-1; i > api_data.length-11; i--) {
-            let col = color(random(100,255), random(100,255), random(100,255));
+    if (api_data.length > 0 && reddit_data.children.length > 0) {
+        for (let i = api_data.length - 1; i > api_data.length - 11; i--) {
+            let col = color(
+                random(100, 255),
+                random(100, 255),
+                random(100, 255)
+            );
             stroke(col);
             fill(col);
-            text(api_data[i].name, random(0, width-50), random(0, height-50));
+            text(
+                api_data[i].name,
+                random(0, width - 50),
+                random(0, height - 50)
+            );
+        }
+
+        for (let i = 0; i < reddit_data.children.length; i++) {
+            let offset = 10;
+            let col = color(255, 100, 100);
+            stroke(col);
+            ellipse(i * offset, height / 2, 5, 5);
         }
         noLoop();
     }
--- server.js
+++ server.js
@@ -1,19 +1,20 @@
 // Environment information
-require('dotenv').config();
+require("dotenv").config();
 // Using express: http://expressjs.com/
-const express = require('express');
+const express = require("express");
 // Create the app
 const app = express();
+const rp = require("request-promise");
 
 // Helper function to save data from response
 const saveData = (jsonData) => {
-  console.log('Saving it on disk because CoinMarketCap are cheap.');
-  let fs = require('fs');
-  fs.writeFile("test.json", jsonData, function (err) {
-    if (err) {
-      console.log(err);
-    }
-  });
+    console.log("Saving it on disk because CoinMarketCap are cheap.");
+    let fs = require("fs");
+    fs.writeFile("test.json", jsonData, function (err) {
+        if (err) {
+            console.log(err);
+        }
+    });
 };
 
 // Set up the server
@@ -22,48 +23,77 @@ const server = app.listen(process.env.PORT || 3000, listen);
 
 // This call back just tells us that the server has started
 function listen() {
-  let host = server.address().address;
-  let port = server.address().port;
-  console.log('Example app listening at http://' + host + ':' + port);
+    let host = server.address().address;
+    let port = server.address().port;
+    console.log("Example app listening at http://" + host + ":" + port);
 }
 
-const rp = require('request-promise');
-const requestOptions = {
-  method: 'GET',
-  uri: '',
-  qs: {
-    'start': '1',
-    'limit': '5000',
-    'convert': 'USD'
-  },
-  headers: {
-    'X-CMC_PRO_API_KEY': process.env.COIN_MARKET_CAP_API
-  },
-  json: true,
-  gzip: true
-};
-
-app.get('/data', (req, res) => {
-  requestOptions.uri = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?sort=name';
-  console.log('This is the request', requestOptions);
-  rp(requestOptions).then(response => {
-    // console.log('Attempting to save response');
-    // saveData(response.json());
-    res.send(response); // send response to sketch.js (the client)
-  }).catch((err) => {
-    console.log('API call error:', err.message);
-  });
+//REDDIT------------------------------------------------------------------------------------------------
+app.get("/reddit", (req, res) => {
+    let subreddit = req.query.subreddit;
+    const requestOptions = {
+        method: "GET",
+        uri: "https://reddit.com/r/" + subreddit + ".json",
+        json: true,
+    };
+    console.log("This is the request", requestOptions);
+    rp(requestOptions)
+        .then((response) => {
+            // console.log('Attempting to save response');
+            // saveData(response.json());
+            res.send(response); // send response to sketch.js (the client)
+        })
+        .catch((err) => {
+            console.log("API call error:", err.message);
+        });
 });
 
+//END REDDIT--------------------------------------------------------------------------------------------
+
+//COINMARKETCAP-----------------------------------------------------------------------------------------
+
+app.get("/coinmarket", (req, res) => {
+    console.log(req.query);
+    let sorting = req.query.sort;
+    const requestOptions = {
+        method: "GET",
+        uri:
+            "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?sort=" +
+            sorting,
+        qs: {
+            start: "1",
+            limit: "5000",
+            convert: "USD",
+        },
+        headers: {
+            "X-CMC_PRO_API_KEY": process.env.COIN_MARKET_CAP_API,
+        },
+        json: true,
+        gzip: true,
+    };
+
+    console.log("This is the request:", requestOptions);
+
+    rp(requestOptions)
+        .then((response) => {
+            // console.log('Attempting to save response');
+            // saveData(response.json());
+            res.send(response); // send response to sketch.js (the client)
+        })
+        .catch((err) => {
+            console.log("API call error:", err.message);
+        });
+});
+//END COINMARKETCAP-------------------------------------------------------------------------------------
+
 // use as home ('/')
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 /* This part should request the data from API */
 // "http://localhost:3001/data"
 // "http://localhost:3001/data?q=veganska-fiskpinnar"
 // "http://google.com/search?q=veganska-fiskpinnar"
 
-// Requests from sketch.js
+// Requests from sketch.jst
 // "http://localhost:3001/data?coin=btc"
 // -------------------------
-
