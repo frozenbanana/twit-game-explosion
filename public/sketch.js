8966fa7cd0d3499ef5bb40d353fc156df0bb0eb0
henrybergstrom <henrybergstrom@protonmail.com>
2020-04-28 16:10:04 +0200

    Our first visual

--- public/sketch.js
+++ public/sketch.js
@@ -27,17 +27,26 @@ const getApiData = async (api_url) => {
 
 console.log("Trying to get data...");
 
+// Custom interface to raw api data
+coinmarketTop10 = [];
+
 getApiData("coinmarket")
     .then((data) => {
         console.log("getApiData::Successfully retreived data:", data);
         api_data = data;
-        normalizeData(api_data);
+
+        api_data.forEach(dataAtTimestamp => {
+            coinmarketTop10.push(dataAtTimestamp.data.slice(0,10));
+        });
+
+        // coinmarketTop10[0].quote.volume_24h
+        // coinmarketTop10[0][0].quote.USD.volume_24h
+        //normalizeData(api_data); // TODO
     })
     .catch((err) => {
         console.log("getApiData::Failed to retreive data:", err);
     });
 
-/*
 getApiData("reddit?subreddit=bitcoin")
     .then((data) => {
         console.log("We got the data", data);
@@ -50,13 +59,13 @@ getApiData("reddit?subreddit=bitcoin")
         // api_data = getDiskData("data/data.json");
     });
 
-*/
+
 //-------------------------------------------------------------------------------------------------
 
 
-let normalizeData = (data) => {
+// let normalizeData = (data) => {
     
-};
+// };
 //-------------------------------------------------------------------------------------------------
 function setup() {
     createCanvas(640, 400);
@@ -68,7 +77,7 @@ function draw() {
     //console.log("The data is: ", data);
     // let x = random(0, width);
     // let y = random(0, height);
-    console.log("data is: ", api_data);
+    console.log("data is: ", coinmarketTop10);
     if (api_data.length > 0) {
         for (let i = 0; i < 10; i++) {
             let col = color(
@@ -78,11 +87,21 @@ function draw() {
             );
             stroke(col);
             fill(col);
+            let x = random(0, width - 50);
+            let y = random(0, height - 50);
+            let normFact = coinmarketTop10[0][0].quote.USD.volume_24h;
+            ellipse(x,y, 50* coinmarketTop10[0][i].quote.USD.volume_24h/normFact);
+
+
+            stroke(127);
+
             text(
-                api_data[0].data[i].name,
-                random(0, width - 50),
-                random(0, height - 50)
+                coinmarketTop10[0][i].name,
+                x,
+                y,
             );
+            
+            
         }
         
         noLoop();
