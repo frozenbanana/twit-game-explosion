c3694c8e0762f7c9fcfa5a2ab7bac697eae8710a
Hoxbot <hoxbot@unseen.is>
2020-05-22 12:36:04 +0200

    Split things into separate functions Added comment counter

--- public/sketch_ValueVsComments.js
+++ public/sketch_ValueVsComments.js
@@ -24,18 +24,18 @@ getApiData("coinmarket")
     .then((data) => {
         console.log("getApiData::Successfully retreived coinmarket data:", data);
         api_data = data;
-    
-        
+
+
         // console.log('price references: ', prices_references);
         api_data.forEach(dataAtTimestamp => {
             let coinsByVolume = dataAtTimestamp.data;
-            
+
             let coins = [];
-            
+
 
             // TODO, use this object to filter top10.length so coinObjects are placed
             // in the right index.
-            
+
             let coinsIdentifier = {
                 tether: 825,
                 bitcoin: 1,
@@ -48,7 +48,7 @@ getApiData("coinmarket")
                 ripple: 52,
                 tron: 1958
             };
-            
+
 
             for (let i = 0; i < Object.keys(coinsIdentifier).length; i++) {
                 // Interface
@@ -62,7 +62,7 @@ getApiData("coinmarket")
                 };
                 const coinKey = Object.keys(coinsIdentifier)[i];
                 let selectedCoin = coinsByVolume.find(coin => coin.id == coinsIdentifier[coinKey]);
-                coinObj.name = selectedCoin.name;    
+                coinObj.name = selectedCoin.name;
                 coinObj.price = selectedCoin.quote.USD.price;
                 coinObj.price_reference = prices_references[i];
                 coinObj.timestamp = new Date(selectedCoin.last_updated).getTime() / 1000;
@@ -79,17 +79,17 @@ getApiData("coinmarket")
     });
 
 //Get Reddit Data
-let redditPosts = {    
-    tether:             [],
-    bitcoin:            [],
-    ethereum:           [],
-    litecoin:           [],
-    bitcoincash:        [],
-    eos:                [],
-    ripple:             [],
-    bitcoinsv:          [],
-    ethereumclassic:    [],
-    tron:               [],
+let redditPosts = {
+    tether: [],
+    bitcoin: [],
+    ethereum: [],
+    litecoin: [],
+    bitcoincash: [],
+    eos: [],
+    ripple: [],
+    bitcoinsv: [],
+    ethereumclassic: [],
+    tron: [],
 };
 
 getApiData("reddit/")
@@ -99,10 +99,10 @@ getApiData("reddit/")
         //Go through the different currencies
 
         let coinKeys = Object.keys(data);
-        
-        for(let i = 0; i < coinKeys.length; i++) {
+
+        for (let i = 0; i < coinKeys.length; i++) {
             const currentCoin = data[coinKeys[i]];
-            for(let date_index = 0; date_index < currentCoin.length; date_index++) {
+            for (let date_index = 0; date_index < currentCoin.length; date_index++) {
                 let postObj = {
                     timestamp: null,
                     num_comments: null
@@ -145,41 +145,44 @@ redditPosts:      [currency][post] {
 //-----HELPER FUNCTIONS ----------------------------------------------------------------------
 
 function setup() {
-    canvasX = 1500;
-    canvasY = 4000;
+
+    //HAI
+
+    canvasX = 3000;
+    canvasY = 8000;
     createCanvas(canvasX, canvasY);
     background(0);
-    textSize(10);  
+    textSize(10);
 
     // Assignment of global variables
-    baseX = 50;
-    baseY = 50;
+    baseX = 100;
+    baseY = 100;
     //topWidth = width - 2 * baseX;
     //topHeight = height - 2 * baseY;
 }
 
 function drawPointAndLine(point_x, point_y, prev_point_x, prev_point_y) {
-    
+
     stroke(color('yellow'));
     strokeWeight(2);
     line(point_x, point_y, prev_point_x, prev_point_y);
 
     let radius = 5;
 
-    stroke(color('yellow'));
-    fill(color('yellow'));
-    //strokeWeight(3);
-    //ellipse(point_x, point_y, radius);
+    stroke(color('black'));
+    fill(color('black'));
+    strokeWeight(1);
+    ellipse(point_x, point_y, radius);
     //ellipse(prev_point_x, prev_point_y, radius);
 
     strokeWeight(1);
 }
 
 function drawGraphAxes(name, name_y, origo_x, origo_y, max_x, max_y) {
-    
+
     stroke(color('white'));
     fill(color('white'));
-    
+
     //X axis
     rect(origo_x, origo_y, max_x, 1);
 
@@ -187,11 +190,11 @@ function drawGraphAxes(name, name_y, origo_x, origo_y, max_x, max_y) {
     rect(origo_x, origo_y, 1, -max_y);
 
     //Text
+    textSize(40);
+    text(name, origo_x + (max_x / 2), origo_y - max_y - 50);
     textSize(20);
-    text(name, origo_x + (max_x/2), origo_y - max_y - 25);
-    textSize(10);
-    text("Date", origo_x + max_x + 10, origo_y + 5);
-    text(name_y, origo_x - 10, origo_y - max_y - 10);
+    text("Date", origo_x + max_x + 20, origo_y + 5);
+    text(name_y, origo_x - 40, origo_y - max_y - 20);
 
 }
 
@@ -199,23 +202,23 @@ function markXAxis(date, pos_x, pos_y_0) {
 
     stroke(color('white'));
     fill(color('white'));
-    rect(pos_x, (pos_y_0 - 5), 1, 10);
+    rect(pos_x, (pos_y_0 - 10), 1, 20);
 
-    textSize(10);
-    text((date[0] + "/" + date[1]), (pos_x-10), (pos_y_0 + 15));
+    textSize(20);
+    text((date[0] + "/" + date[1]), (pos_x - 20), (pos_y_0 + 30));
 }
 
-function markYAxis(pos_x, pos_y_0, y_axis_len, scaling_mod, max_price, min_price) {
+function markYAxisPrice(pos_x, pos_y_0, y_axis_len, scaling_mod, max_price, min_price) {
 
     //Calculate middle value
-    let mid_price = (max_price + min_price)/2;
+    let mid_price = (max_price + min_price) / 2;
 
     //Go from 1000000 to 0.000001 and figure out the magnitude of the number
     let magnitude = 1000000;
     let fix = 0;
-    for(let i = 6; i > -6; i--) {
-        if(i < 0){ fix = abs(i); }
-        if((mid_price / magnitude) >= 1) { i = -7; } //Break loop
+    for (let i = 6; i > -6; i--) {
+        if (i < 0) { fix = abs(i); }
+        if ((mid_price / magnitude) >= 1) { i = -7; } //Break loop
         else { magnitude /= 10; }   //Elsewise divide by 10
     }
 
@@ -225,133 +228,227 @@ function markYAxis(pos_x, pos_y_0, y_axis_len, scaling_mod, max_price, min_price
     //Draw lines and value numbers
     stroke(color('white'));
     fill(color('white'));
-    textSize(10);
+    textSize(20);
     textAlign(RIGHT);
 
-    for(let y = 0; y < y_axis_len; y += magnitude*scaling_mod){
-        rect(pos_x-5, (pos_y_0 - y), 10, 1);
+    for (let y = 0; y < y_axis_len; y += magnitude * scaling_mod) {
+        rect(pos_x - 10, (pos_y_0 - y), 20, 1);
 
-        let price = Math.round((y/scaling_mod)/magnitude)*magnitude;
+        let price = Math.round((y / scaling_mod) / magnitude) * magnitude;
         //console.log(price);
-        text(price.toFixed(fix), pos_x-10, pos_y_0-y+5);
+        text(price.toFixed(fix), pos_x - 20, pos_y_0 - y + 10);
     }
 
     textAlign(LEFT);
 }
 
+function markYAxisCommens(pos_x, pos_y_0, y_axis_len, scaling_mod, max_comments) {
+    //bloop
+}
+
+function bigDrawValueTime(num_of_dates, num_of_currencies, x_axis_len, y_axis_len) {
+    //STATISTICS
+    let max_price = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    let inf = Number.POSITIVE_INFINITY;
+    let min_price = [inf, inf, inf, inf, inf, inf, inf, inf, inf, inf];
+    for (let curr_iter = 0; curr_iter < num_of_currencies; curr_iter++) {
+        for (let date_iter = 0; date_iter < num_of_dates; date_iter++) {
+            if (max_price[curr_iter] < coinmarketTop10[date_iter][curr_iter].price) {
+                max_price[curr_iter] = coinmarketTop10[date_iter][curr_iter].price;
+            }
+            if (min_price[curr_iter] > coinmarketTop10[date_iter][curr_iter].price) {
+                min_price[curr_iter] = coinmarketTop10[date_iter][curr_iter].price;
+            }
+        }
+    }
+    //console.log("max", max_price);
+    //console.log("min", min_price);
+    //
+
+    //DRAW LINEGRAPH SHOWING VALUE OVERTIME (x:dates, y:value)
+    for (let curr_iter = 0; curr_iter < num_of_currencies; curr_iter++) {
+        //
+        drawGraphAxes(
+            coinmarketTop10[0][curr_iter].name,
+            "Value (USD)",
+            baseX,
+            (canvasY / (num_of_currencies + 1)) * (curr_iter + 1),
+            x_axis_len,
+            y_axis_len,
+        );
+
+        let scaling_mod = y_axis_len / (2 * max_price[curr_iter]);
+        let y_axis_0 = (canvasY / (num_of_currencies + 1)) * (curr_iter + 1);
+
+        markYAxisPrice(
+            baseX,
+            y_axis_0,
+            y_axis_len,
+            scaling_mod,
+            max_price[curr_iter],
+            min_price[curr_iter]
+        );
+
+        //
+        let prev_x = baseX;
+        let prev_y = y_axis_0;
+        prev_y -= coinmarketTop10[0][curr_iter].price * scaling_mod;
+
+        let last_date = coinmarketTop10[0][curr_iter].date;
+        for (let date_iter = 0; date_iter < num_of_dates; date_iter++) {
+            //x
+            let x = baseX + date_iter * (x_axis_len / num_of_dates);
+
+            //y = base_y - price*mod
+            let y = y_axis_0;
+            y -= coinmarketTop10[date_iter][curr_iter].price * scaling_mod;
+
+            drawPointAndLine(
+                x,
+                y,
+                prev_x,
+                prev_y
+            );
+
+            if (last_date[0] != coinmarketTop10[date_iter][curr_iter].date[0]) {
+                last_date = coinmarketTop10[date_iter][curr_iter].date;
+                markXAxis(last_date, x, y_axis_0);
+            }
+
+            prev_x = x;
+            prev_y = y;
+        }
+    }
+}
+
+function bigDrawCommentTime(num_of_dates, num_of_currencies, x_axis_len, y_axis_len) {
+    //DRAW LINEGRAPH SHOWING COMMENTS OVERTIME (x:dates, y:comments)
+
+    //COUNT COMMENTS IN INTERVALS
+    let crypto_Date_Reached = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]; // to keep track of each
+    //For all dates
+    for (let date_index = 0; date_index < coinmarketTop10.length; date_index++) {
+
+        //For each currency
+        for (let currency_index = 0; currency_index < coinmarketTop10[date_index].length; currency_index++) {
+            //Read reddit comments that happened before current date
+            //Once current date is reached, stop and move to next date
+            let curr_key = Object.keys(redditPosts)[currency_index];
+
+            while (redditPosts[curr_key][crypto_Date_Reached[currency_index]] &&
+                redditPosts[curr_key][crypto_Date_Reached[currency_index]].timestamp < coinmarketTop10[date_index][currency_index].timestamp) {
+                //console.log("Comments in Interval", curr_key, coinmarketTop10[date_index][currency_index].timestamp); //To see the how many comments have the same timestamps        
+                coinmarketTop10[date_index][currency_index].comments_in_interval += 1 + redditPosts[curr_key][crypto_Date_Reached[currency_index]].num_comments;
+
+                crypto_Date_Reached[currency_index]++;
+            }
+            //console.log("Comments in Interval", curr_key, coinmarketTop10[date_index][currency_index].comments_in_interval); //Comments in each interval
+        }
+    }
+
+    //STATISTICS
+    let max_comments = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    //let inf = Number.POSITIVE_INFINITY;
+    //let min_price = [ inf, inf, inf, inf, inf, inf, inf, inf, inf, inf ];
+    for (let curr_iter = 0; curr_iter < num_of_currencies; curr_iter++) {
+        for (let date_iter = 1; date_iter < num_of_dates; date_iter++) {
+            if (max_comments[curr_iter] < coinmarketTop10[date_iter][curr_iter].comments_in_interval) {
+                max_comments[curr_iter] = coinmarketTop10[date_iter][curr_iter].comments_in_interval;
+            }
+            //if(min_price[curr_iter] > coinmarketTop10[date_iter][curr_iter].price){
+            //    min_price[curr_iter] = coinmarketTop10[date_iter][curr_iter].price;
+            //}
+        }
+    }
+
+    //
+
+    for (let curr_iter = 0; curr_iter < num_of_currencies; curr_iter++) {
+
+        drawGraphAxes(
+            coinmarketTop10[0][curr_iter].name,
+            "Nr of Posts/Comments",
+            (canvasX / 2) + baseX,
+            (canvasY / (num_of_currencies + 1)) * (curr_iter + 1),
+            x_axis_len,
+            y_axis_len,
+        );
+
+        let scaling_mod = y_axis_len / (max_comments[curr_iter]);
+        let y_axis_0 = (canvasY / (num_of_currencies + 1)) * (curr_iter + 1);
+        /*
+        markYAxisComments(
+            (canvasX/2) + baseX,
+            y_axis_0,
+            y_axis_len,
+            scaling_mod,
+            max_comments[curr_iter]
+        );
+        */
+       drawPointAndLine(
+            (canvasX / 2) + baseX + x_axis_len,
+            y_axis_0 - 1 * scaling_mod,
+            (canvasX / 2) + baseX,
+            y_axis_0 - 1 * scaling_mod
+        );
+        //
+        let prev_x = (canvasX / 2) + baseX;
+        let prev_y = y_axis_0;
+        prev_y -= coinmarketTop10[1][curr_iter].comments_in_interval * scaling_mod;
+
+        let last_date = coinmarketTop10[1][curr_iter].date;
+        for (let date_iter = 1; date_iter < num_of_dates; date_iter++) {
+            //x
+            let x = (canvasX / 2) + baseX + date_iter * (x_axis_len / num_of_dates);
+
+            //y = base_y - price*mod
+            let y = y_axis_0;
+
+            y -= coinmarketTop10[date_iter][curr_iter].comments_in_interval * scaling_mod;
+
+            /*
+            drawPointAndLine(
+                x,
+                y,
+                prev_x,
+                prev_y
+            );
+            */
+            stroke(color('black'));
+            fill(color('#9C6ECC'));
+            rect(x, y_axis_0, 4, -coinmarketTop10[date_iter][curr_iter].comments_in_interval * scaling_mod);
+
+            if (last_date[0] != coinmarketTop10[date_iter][curr_iter].date[0]) {
+                last_date = coinmarketTop10[date_iter][curr_iter].date;
+                markXAxis(last_date, x, y_axis_0);
+            }
+
+            prev_x = x;
+            prev_y = y;
+        }//date_iter
+    }//curr_iter
+}
+
 function draw() {
     console.log("draw() called.");
-    
-    if (coinmarketTop10.length > 0) {
+
+    if (coinmarketTop10.length > 0 && redditPosts['bitcoin'].length > 0) {
         console.log(`We got data. coinmarket: ${coinmarketTop10.length}, reddit: ${Object.keys(redditPosts).length}`);
 
         stroke(color('white'));
         fill(color('white'));
-        rect(canvasX/2, 0, 1, canvasY);
+        rect(canvasX / 2, 0, 1, canvasY);
 
         let num_of_dates = coinmarketTop10.length;
         let num_of_currencies = coinmarketTop10[0].length;
 
-        //STATISTICS
-        let max_price = [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ];
-        let inf = Number.POSITIVE_INFINITY;
-        let min_price = [ inf, inf, inf, inf, inf, inf, inf, inf, inf, inf ];
-        for(let curr_iter = 0; curr_iter < num_of_currencies; curr_iter++){
-            for(let date_iter = 0; date_iter < num_of_dates; date_iter++){
-                if(max_price[curr_iter] < coinmarketTop10[date_iter][curr_iter].price){
-                    max_price[curr_iter] = coinmarketTop10[date_iter][curr_iter].price;
-                }
-                if(min_price[curr_iter] > coinmarketTop10[date_iter][curr_iter].price){
-                    min_price[curr_iter] = coinmarketTop10[date_iter][curr_iter].price;
-                }
-            }
-        }
-        console.log("max", max_price);
-        console.log("min", min_price);
-        //
+        let x_axis_len = canvasX / 2 - 2 * baseX;
+        let y_axis_len = (canvasY / (num_of_currencies + 1)) - 2 * baseY;
 
-        //DRAW AXES
-        let x_axis_len = canvasX/2 - 2*baseX;
-        let y_axis_len = (canvasY/(num_of_currencies+1)) - 2*baseY;
+        bigDrawValueTime(num_of_dates, num_of_currencies, x_axis_len, y_axis_len);
+        bigDrawCommentTime(num_of_dates, num_of_currencies, x_axis_len, y_axis_len);
 
-        for(let curr_iter = 0; curr_iter < num_of_currencies; curr_iter++){
-            drawGraphAxes(
-                coinmarketTop10[0][curr_iter].name,
-                "Value (USD)",
-                baseX,
-                (canvasY/(num_of_currencies+1)) * (curr_iter+1),
-                x_axis_len,
-                y_axis_len, 
-            );
-            
-            drawGraphAxes(
-                coinmarketTop10[0][curr_iter].name,
-                "Nr of Posts/Comments",
-                (canvasX/2)+baseX,
-                (canvasY/(num_of_currencies+1)) * (curr_iter+1),
-                x_axis_len,
-                y_axis_len,
-            );
-        }
-
-        //DRAW LINEGRAPH SHOWING VALUE OVERTIME (x:dates, y:value)
-        for(let curr_iter = 0; curr_iter < num_of_currencies; curr_iter++){
-            //
-            let scaling_mod = y_axis_len / (2*max_price[curr_iter]);
-            let y_axis_0 = (canvasY/(num_of_currencies+1)) * (curr_iter+1);
-
-            markYAxis(
-                baseX,
-                y_axis_0,
-                y_axis_len,
-                scaling_mod,
-                max_price[curr_iter],
-                min_price[curr_iter]
-            );
-
-            //
-            let prev_x = baseX;
-            let prev_y = y_axis_0;
-            prev_y -= coinmarketTop10[0][curr_iter].price * scaling_mod;
-
-            let last_date = coinmarketTop10[0][curr_iter].date;
-            for(let date_iter = 0; date_iter < num_of_dates; date_iter++){
-                //x
-                let x = baseX + date_iter*(x_axis_len/num_of_dates);
-
-                //y = base_y - price*mod
-                let y = y_axis_0;
-                y -= coinmarketTop10[date_iter][curr_iter].price * scaling_mod;
-
-                drawPointAndLine(
-                    x, 
-                    y, 
-                    prev_x, 
-                    prev_y
-                );
-
-                if(last_date[0] != coinmarketTop10[date_iter][curr_iter].date[0]) {
-                    last_date = coinmarketTop10[date_iter][curr_iter].date;
-                    markXAxis(last_date, x, y_axis_0);
-                }
-
-                prev_x = x;
-                prev_y = y;
-            }
-        }
-        //drawPointAndLine(20, 20, 100, 100);
-
-        /*textSize(20);
-        stroke(color('white'));
-        fill(color('white'));
-        text("Percentual change in value over 1 hour", (topWidth/2)-3*baseX, 25);
-        */
-       
-        //DRAW LINEGRAPH SHOWING COMMENTS OVERTIME (x:dates, y:comments)
-        
-
-
-        noLoop();   
+        noLoop();
     }
-    
+
 }
